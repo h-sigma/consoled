b9349bb6e49bbecaa765051824752c2a5a08a0a6
h-sigma <harsh@aka.al>
2021-04-08 18:41:28 +0530

    Moved resources to proper sub-dire

---
+++ Akaal/Consoled/Editor/EditorGUI/ConsoledResources.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Akaal.Consoled.Editor
+{
+    public static class ConsoledResources
+    {
+        public static VisualTreeAsset WindowAsset()
+        {
+            return Resources.Load<VisualTreeAsset>("Consoled/ConsoledWindow_UXML");
+        }
+
+        public static StyleSheet StyleSheet()
+        {
+            return Resources.Load<StyleSheet>("Consoled/ConsoledWindow_USS");
+        }
+
+        public static Texture2D LogoTexture()
+        {
+            return Resources.Load<Texture2D>("Consoled/ConsoledLogo");
+        }
+    }
+}
